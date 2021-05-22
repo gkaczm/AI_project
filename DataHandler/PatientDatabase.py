cb583d7ac93d61b03c044f8c57432172d47c82f3
Mikolaj <lupsik@wp.pl>
2021-05-22 16:12:34 +0200

    added a youtube link to understand the qrs axis angles

--- DataHandler/PatientDatabase.py
+++ DataHandler/PatientDatabase.py
@@ -5,15 +5,17 @@ class PatientDatabase:
     def __init__(self, data_path):
         self.all_patient_data = pd.read_csv(data_path)
 
+        # Columns which must not be NaN or the corresponding row will be removed from database
         self.mandatory_columns = ['QRSduration', 'PRinterval', 'Q-Tinterval', 'Tinterval', 'Pinterval',
-                                  'J']  # Kolumny które są wymagane do zaakceptowania danych
-
+                                  'J']
         self.discard_empty()
 
     def discard_empty(self):
+        print("Patient count before pruning : ", self.all_patient_data.count())
         self.all_patient_data.dropna(subset=self.mandatory_columns,
                                      inplace=True,
                                      how='any')
+        print("Patient count after pruning : ", self.all_patient_data.count())
 
     def print_first_n(self, n):
         print(self.all_patient_data.head(n).to_string())
