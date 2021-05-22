f517e39bf0763d1c30d3422351317016bae28060
Mikolaj <lupsik@wp.pl>
2021-05-22 16:06:57 +0200

    added a youtube link to understand the qrs axis angles

--- DataHandler/Patient.py
+++
@@ -1,7 +0,0 @@
-class Patient:
-
-    def __init__(self, age, sex, height, weight, cardiac_data):
-        self.age = age
-        self.sex = sex  # 0 = male; 1 = female
-        self.height = height  # cm
-        self.weight = weight  # kg
--- DataHandler/PatientDatabase.py
+++ DataHandler/PatientDatabase.py
@@ -1,3 +1,19 @@
+import pandas as pd
 
 
-class CardiacDatabase:
+class PatientDatabase:
+    def __init__(self, data_path):
+        self.all_patient_data = pd.read_csv(data_path)
+
+        self.mandatory_columns = ['QRSduration', 'PRinterval', 'Q-Tinterval', 'Tinterval', 'Pinterval',
+                                  'J']  # Kolumny które są wymagane do zaakceptowania danych
+
+        self.discard_empty()
+
+    def discard_empty(self):
+        self.all_patient_data.dropna(subset=self.mandatory_columns,
+                                     inplace=True,
+                                     how='any')
+
+    def print_first_n(self, n):
+        print(self.all_patient_data.head(n).to_string())
