eda82fd19ac07bd78b8f373d0f57cf87ab3e770e
Mikolaj <lupsik@wp.pl>
2021-05-22 14:37:17 +0200

    added a youtube link to understand the qrs axis angles

--- DataHandler/CardiacData.py
+++ DataHandler/CardiacData.py
@@ -1,14 +1,14 @@
+import pandas as pd
+import numpy as np
+import os
+
+
 class CardiacData:
 
-    def __init__(self, qrs_dur, pr_int, qt_int, t_int, p_int, qrs, t, p, qrst, j):
-        self.qrs_duration = qrs_dur
-        self.pr_interval = pr_int
-        self.qt_interval = qt_int
-        self.t_int = t_int
-        self.p_interval = p_int
+    def __init__(self, data_path):
+        cardiac_data = pd.read_csv(data_path)
+        print(cardiac_data)
 
-        self.qrs_vector_angle = qrs
-        self.t_vector_angle = t
-        self.p_vector_angle = p
-        self.qrst_vector_angle = qrst
-        self.j_vector_angle = j
+dir = os.path.realpath(os.path.dirname(__file__))  # Directory of your Python file
+file_path = os.path.join(dir, "resources", "arrhythmia.csv")  # Create the path of the file
+test = CardiacData(file_path)
